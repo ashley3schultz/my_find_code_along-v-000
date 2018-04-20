53bf6fee2e7142727ad89074ac2b2efda926fdc0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-04-20 21:02:14 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_find(collection)
   len = 0 
   while len < collection.size 
-    if yield(collection[len])
-      return collection[len]
+    return collection[len]if yield(collection[len])
+      
     end
     len += 1 
   end
